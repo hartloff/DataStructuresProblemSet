672edd64c91a3bb22d239758404d372735fabad5
jessehartloff <hartloff@buffalo.edu>
2017-02-20 13:14:20 -0500

    add handout

---
+++ src/PS4_DataStructures.java
@@ -0,0 +1,104 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class PS4_DataStructures {
+
+
+    static int Q1(ArrayList<Integer> listOfNumbers){
+        // return the number of integers in listOfNumbers
+
+        return 0;
+    }
+
+    static void Q2(ArrayList<String> input){
+        // add the String "movie" to the end of the input list
+    }
+
+    static int Q3(ArrayList<Integer> input, int index){
+        // return the element at index i from the input list
+        // You can assume the input contains at least i+1 values
+
+        return 0;
+    }
+
+    static boolean Q4(ArrayList<Integer> input, int value){
+        // return true if value is in the input list, false otherwise
+
+        return false;
+    }
+
+    static String Q5(ArrayList<String> words){
+        // return a string containing every word of the input concatenated into a single String and separated by spaces
+        // Example: If the input is ["war", "never", "changes"] the output is "war never changes"
+
+        return "";
+    }
+
+    static ArrayList<Integer> Q6(){
+        // Create and return an ArrayList of the integers 1 through 10 (including 1 and 10)
+
+        return null;
+    }
+
+    static ArrayList<String> Q7(){
+        // create and return an ArrayList of Strings containing the strings "memory", "is", "the", "key" in that order
+
+        return null;
+    }
+
+    static void Q8(HashMap<String, Integer> input, String key, int value){
+        // add an entry into the map with the the given key and value
+
+    }
+
+    static boolean Q9(HashMap<Integer, Boolean> input, int key){
+        // return the value in the input map at the given key
+
+        return true;
+    }
+
+    static boolean Q10(HashMap<String, Integer> input, int value){
+        // return true if value is in the input map as a value at any key, false otherwise
+
+        return true;
+    }
+
+    static int Q11(HashMap<String, Double> input){
+        // return the number of entries in the input map
+
+        return 0;
+    }
+
+    static ArrayList<Integer> Q12(HashMap<String, Integer> input){
+        // return a list of all values in the input map
+        // Note: input.values() returns a Collection, but you must return an ArrayList
+
+        return null;
+    }
+
+    static ArrayList<String> Q13(HashMap<String, Integer> input){
+        // return a list of all keys in the input map
+        // Note: input.keySet() returns a Set, but you must return an ArrayList
+
+        return null;
+    }
+
+    static boolean Q14(HashMap<Integer, Integer> input, int key){
+        // return true if key is a key in the input map, false otherwise
+
+        return true;
+    }
+
+    static int Q15(HashMap<Integer, Integer> input, int value){
+        // return the number of times value is in the input map as a value
+
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
